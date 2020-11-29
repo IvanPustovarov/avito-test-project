04f1729ac9a4325e310dbf56f5ede498c6609603
Winston Smith <silverflash150@gmail.com>
2020-11-29 15:27:19 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+//components
+import MainPage from './components/MainPage';
+import NewsPage from './components/NewsPage';
+
+function App() {
+  return (
+    <div className="App">
+     <MainPage/>
+     <NewsPage/>
+    </div>
+  );
+}
+
+export default App;
